dddaafca4210bcfc68e13549b42d06c809dae4ae
zirra <msullivan@heysulli.com>
2025-09-19 17:22:13 -0500

    signup form

--- src/components/component/SignupComponent.vue
+++ src/components/component/SignupComponent.vue
@@ -3,8 +3,12 @@
     <image-item 
       :context="{url: 'https://landerpop.s3.us-east-1.amazonaws.com/lpuEkSePpwewsvqsCtzvqRMz/PrairieBandLogo.png'}"
       class="form-head"/>
-    <h1>Sign In</h1>
-    <p>To access and participate in any of the offers and awards from our games a phone number and email is required.  Once added you will not be asked for this information again.</p><br/><br/>
+
+    <h1>Sign Up</h1>
+
+    <p><strong>How it works:</strong> Awards are linked to the mobile number that received the promotion. They can only be redeemed in person at the location that issued the promotion.</p>
+    <br/>
+
     <input-field 
       @inputupdated="updateField"
       :context="{placeholder: 'Mobile Number', id: 'mobile'}"
@@ -13,8 +17,16 @@
       @inputupdated="updateField"
       :context="{placeholder: 'Email Address', id: 'email'}"
       key="email1" />
+
+    <br/>
     <button>SUBMIT</button>
-    <p class="smaller">Opting in with your mobile number allows us to send you special promotions that are linked directly to your number. Only you can redeem them, in person, at our location. No cash value</p>
+    <br/><br/>
+    
+    <p><strong>“Standard terms apply. Non-transferable. No cash value. See full T&Cs.”</strong></p>
+    <br/>
+    <p class="note">For the complete legal <a href="https://landerpop.s3.us-east-1.amazonaws.com/legal/TermsConditions.html" target="_blank">Terms & Conditions</a>, please visit our website.</p>
+    
+
   </div>
 </template>
 
